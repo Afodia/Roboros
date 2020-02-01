b9ac7e8ef6852b73d3c6a66d4e7f4064ce98146d
Caelou <lucas.zimmer@epitech.eu>
2020-02-01 14:45:51 +0100

    [+] script to play video

---
+++ Roboros/Assets/Scripts/playCutscene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playCutscene : MonoBehaviour
+{
+    public GameObject videoPlayer;
+    public int stopVideo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        videoPlayer.SetActive(true);
+        Destroy(videoPlayer, stopVideo);
+    }
+}
