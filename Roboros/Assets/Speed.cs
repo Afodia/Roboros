74f6bbec172303c8c55321eaf13d24def5fcbdfa
Afodia <bourgeay.ch@gmail.com>
2020-02-02 01:18:44 +0100

    lol v7

--- Roboros/Assets/Speed.cs
+++ Roboros/Assets/Speed.cs
@@ -10,7 +10,8 @@ public class Speed : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        instance = this;
+        if (!instance)
+            instance = this;
     }
 
     // Update is called once per frame
